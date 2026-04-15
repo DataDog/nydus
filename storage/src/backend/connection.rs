a2fd14896ca059cb3fae551a2e3cb161bbe66717
Etienne Carriere <etienne.carriere@datadoghq.com>
2026-04-15 10:33:18 +0200

    Handle only TLS trusted CA configuration
    Signed-off-by: Etienne Carriere <etienne.carriere@datadoghq.com>

--- storage/src/backend/connection.rs
+++ storage/src/backend/connection.rs
@@ -76,10 +76,6 @@ pub(crate) struct ConnectionConfig {
     pub retry_limit: u8,
     /// Paths to PEM-encoded CA certificate files to trust in addition to the system CA store.
     pub ca_cert_files: Vec<String>,
-    /// Path to a PEM-encoded client certificate file for mTLS authentication.
-    pub client_cert_file: Option<String>,
-    /// Path to a PEM-encoded private key file for mTLS authentication.
-    pub client_key_file: Option<String>,
 }
 
 impl Default for ConnectionConfig {
@@ -92,8 +88,6 @@ impl Default for ConnectionConfig {
             connect_timeout: 5,
             retry_limit: 0,
             ca_cert_files: Vec::new(),
-            client_cert_file: None,
-            client_key_file: None,
         }
     }
 }
@@ -108,8 +102,6 @@ impl From<OssConfig> for ConnectionConfig {
             connect_timeout: c.connect_timeout,
             retry_limit: c.retry_limit,
             ca_cert_files: c.ca_cert_files,
-            client_cert_file: None,
-            client_key_file: None,
         }
     }
 }
@@ -124,8 +116,6 @@ impl From<S3Config> for ConnectionConfig {
             connect_timeout: c.connect_timeout,
             retry_limit: c.retry_limit,
             ca_cert_files: c.ca_cert_files,
-            client_cert_file: None,
-            client_key_file: None,
         }
     }
 }
@@ -140,8 +130,6 @@ impl From<RegistryConfig> for ConnectionConfig {
             connect_timeout: c.connect_timeout,
             retry_limit: c.retry_limit,
             ca_cert_files: c.ca_cert_files,
-            client_cert_file: c.client_cert_file,
-            client_key_file: c.client_key_file,
         }
     }
 }
@@ -156,8 +144,6 @@ impl From<HttpProxyConfig> for ConnectionConfig {
             connect_timeout: c.connect_timeout,
             retry_limit: c.retry_limit,
             ca_cert_files: Vec::new(),
-            client_cert_file: None,
-            client_key_file: None,
         }
     }
 }
@@ -685,16 +671,6 @@ impl Connection {
             let pem = std::fs::read(ca_cert_file).map_err(|e| einval!(e))?;
             let cert = reqwest::Certificate::from_pem(&pem).map_err(|e| einval!(e))?;
             cb = cb.add_root_certificate(cert);
-        }
-
-        if let (Some(cert_file), Some(key_file)) =
-            (&config.client_cert_file, &config.client_key_file)
-        {
-            let cert_pem = std::fs::read(cert_file).map_err(|e| einval!(e))?;
-            let key_pem = std::fs::read(key_file).map_err(|e| einval!(e))?;
-            let identity =
-                reqwest::Identity::from_pkcs8_pem(&cert_pem, &key_pem).map_err(|e| einval!(e))?;
-            cb = cb.identity(identity);
         }
 
         if !proxy.is_empty() {
